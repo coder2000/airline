9329009befb29bef7a9dd25138190c4466866b4e
Dieter Lunn <coder2000@gmail.com>
2018-07-08 18:21:34 -0500

    Initial commit of new Avalonia MVVM app

---
+++ Airline/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace Airline.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
