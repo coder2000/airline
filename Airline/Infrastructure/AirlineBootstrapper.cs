a2a39f05fcabfa97e6c8e81c2962d4179845e858
Dieter Lunn <coder2000@gmail.com>
2018-07-08 21:05:34 -0500

    Implement bootstrapper abstract

---
+++ Airline/Infrastructure/AirlineBootstrapper.cs
@@ -0,0 +1,16 @@
+using Prism.DryIoc;
+
+namespace Airline.Infrastructure {
+    public class AirlineBootstrapper : DryIocBootstrapper
+    {
+        public override void Run(bool runWithDefaultConfiguration)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void ConfigureServiceLocator()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
